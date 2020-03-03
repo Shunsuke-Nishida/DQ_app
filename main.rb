d20baa9d2eb5f113f2899f724140342238b4217b
Shunsuke-Nishida <hit1103.nishida.aonisai@gmail.com>
2020-03-03 22:33:43 +0900

    ゲッターとセッターの書き換え・initializeメソッドの引数の簡略化

--- main.rb
+++ main.rb
@@ -1,55 +1,32 @@
 class Brave
 
-    # nameのゲッター
-    def name
-        @name
-    end
+    # ゲッターの代わりになる
+    attr_reader :name, :offense, :defense
+    # HP以外は書き換えられると困るので、HPのみセッターも定義
+    attr_accessor :hp
 
-    # nameのセッター
-    def name=(name)
-        @name = name
-    end
-
-    # hpのゲッター
-    def hp
-        @hp
-    end
-
-    # hpのセッター
-    def hp=(hp)
-        @hp = hp
-    end
-
-    # offenseのゲッター
-    def offense
-        @offense
-    end
-
-    # offenseのセッター
-    def offense=(offense)
-        @offense = offense
-    end
-
-    # defenseのゲッター
-    def defense
-        @defense
-    end
-
-    # defenseのセッター
-    def defense=(defense)
-        @defense = defense
+    # newされたときに呼び出される
+    # initializeによりセッターが不要になる
+    # **引数 とすることでハッシュしか受け取れなくなる
+    def initialize(**params)
+        @name = params[:name]
+        @hp = params[:hp]
+        @offense = params[:offense]
+        @defense = params[:defense]
     end
 
 end
 
 # 勇者クラスをインスタンス化
-brave = Brave.new
+brave = Brave.new(name: "テリー",hp: 500,offense: 150,defense: 100)
 
-# パラメーターに値を設定
-brave.name = "テリー"
-brave.hp = 500
-brave.offense = 150
-brave.defense = 100
+# start initializeメソッドにより不要
+# # パラメーターに値を設定
+# brave.name = "テリー"
+# brave.hp = 500
+# brave.offense = 150
+# brave.defense = 100
+# end
 
 puts <<~EOS
     NAME: #{brave.name}
@@ -57,3 +34,7 @@ puts <<~EOS
     OFFENSE: #{brave.offense}
     DEFENSE: #{brave.defense}
 EOS
+
+brave.hp -= 30
+
+puts "#{brave.name}はダメージを受けた！　残りHPは#{brave.hp}だ！"
